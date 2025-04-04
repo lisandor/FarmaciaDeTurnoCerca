d0b3200a8d4da36c6d644436e60dae8b35374b7f
Lisandro Leon Del Real <lisandro.delreal@scba.gov.ar>
2025-04-04 13:37:01 -0300

    v1

---
+++ Modelo/Farmacia.cs
@@ -0,0 +1,12 @@
+﻿using GeoCoordinatePortable;
+
+namespace FarmaciaDeTurnoCerca.Modelo;
+
+public class Farmacia
+{
+    public string Nombre { get; set; }
+    public string Direccion { get; set; }
+    public string Ubicacion { get; set; }
+    public GeoCoordinate Geo { get; set; }
+    public double Distancia { get; set; }
+}
